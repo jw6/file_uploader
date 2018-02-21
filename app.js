7da1f5e8c7b9aea3fc02a481e2b6882cab255938
Jay Wang <jaywang007@yahoo.com>
2018-02-21 17:19:46 -0600

    basic index page

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ const path    = require('path');
 // Init app
 const app = express();
 
+// EJS
+app.set('view engine', 'ejs');
+
+// Public folder
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => res.render('index'));
+
 const port = 3000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
