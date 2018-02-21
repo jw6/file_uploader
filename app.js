7ffac59c2542098798d6a613a95a0d59685d84c2
Jay Wang <jaywang007@yahoo.com>
2018-02-21 15:53:21 -0600

    dev setup

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const ejs     = require('ejs');
+const multer  = require('multer');
+const path    = require('path');
+
+// Init app
+const app = express();
+
+const port = 3000;
